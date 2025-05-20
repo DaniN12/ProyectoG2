d41508a065f5d07886d5ccaed6705534519f6008
Keli112 <kbilbao22@gmail.com>
2025-05-20 20:12:00 +0200

    Cambios en el menu

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const nav = document.getElementById('navMenu');
 toggleButton.addEventListener('click', () => {
     nav.classList.toggle('show');
 });
+
+document.addEventListener('click', (event) => {
+    const isClickInsideNav = nav.contains(event.target);
+    const isClickOnToggle = toggleButton.contains(event.target);
+
+    if (!isClickInsideNav && !isClickOnToggle) {
+        nav.classList.remove('show');
+    }
+});
+
